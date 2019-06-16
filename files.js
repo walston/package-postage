e9d165d17c6503baab239bfef788f2e462fba3f0
Nathan Walston <nathanielwalston@gmail.com>
2019-06-16 08:08:20 -0700

    Check the validity of file pointers package.json

--- files.js
+++ files.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const fs = require("fs");
 
+const KEYS_TO_VALIDATE = ["bin", "main", "module"];
 const COPY_FILES = /^(?:CHANGELOG|changelog|README|readme|LICEN[SC]E|licen[sc]e)(?:\..*)?$/;
 const CWD = process.cwd();
 let destination = path.resolve(CWD, "dist");
@@ -43,7 +44,34 @@ function writePackage(data) {
       if (err) return reject(err);
       resolve();
     });
-  });
+  }).then(() => validateFilePointers(data));
+}
+
+function validateFilePointers(data) {
+  const pkg = JSON.parse(data);
+
+  const exists = rel =>
+    new Promise((resolve, reject) => {
+      fs.stat(path.resolve(destination, rel), (err, stats) => {
+        if (err) return reject(err);
+        if (stats.isFile()) return resolve("OK");
+        return reject("ERR");
+      });
+    });
+
+  return Promise.all([
+    typeof pkg["main"] == "string"
+      ? exists(pkg["main"])
+      : Promise.resolve('No "main" key'),
+    typeof pkg["module"] == "string"
+      ? exists(pkg["module"])
+      : Promise.resolve('No "module" key'),
+    typeof pkg["bin"] == "object"
+      ? Promise.all(
+          Object.values(pkg["bin"]).map(file_path => exists(file_path))
+        )
+      : Promise.resolve('No "bin" key')
+  ]);
 }
 
 /** @param {string} src path to CHANGELOG.md, README.md, LICENSE */
@@ -54,8 +82,6 @@ function copyFilesFrom(src) {
     const src_files = fs
       .readdirSync(directory)
       .filter(value => COPY_FILES.test(value));
-
-    src_files.map(v => JSON.stringify(v)).forEach(console.log);
 
     return Promise.all(
       src_files.map(
